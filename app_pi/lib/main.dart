2e19ccfa658bcba51c4b43835ab0f9d739e469f2
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2021-01-04 00:38:24 +0000

    Adicionei dependencias e atualizei a main.dart

--- app_pi/lib/main.dart
+++ app_pi/lib/main.dart
@@ -1,8 +1,24 @@
-import 'package:app_pi/Home.dart';
 import 'package:flutter/material.dart';
+import 'package:app_pi/view/LoginPage.dart';
 
-void main() {
-  runApp( MaterialApp(
-    home: new Home(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aquisição de Senhas',
+      debugShowCheckedModeBanner: false,
+//      theme: _buildShrineTheme(),
+      theme: ThemeData(
+        primaryColor: Colors.white ,
+        hintColor: Colors.black,
+      ),
+      home: LoginPage(),
+//      debugShowCheckedModeBanner: false,
+    );
+  }
+
+
 }
